595af69e18c3fd8f965e6ae33f6b78e30560cd05
Magnus Axelsson <magsnus26@hotmail.com>
2024-03-29 23:10:02 +0100

    Test config of github pages.

---
+++ docs/_next/static/chunks/app/layout-74de065dfa47a974.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7401:function(n,e,i){Promise.resolve().then(i.bind(i,8757)),Promise.resolve().then(i.t.bind(i,2481,23))},2481:function(){}},function(n){n.O(0,[757,971,69,744],function(){return n(n.s=7401)}),_N_E=n.O()}]);
