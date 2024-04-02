f064f06679b3805d416c0b7fa1be65999dd2b94e
maglet <magsnus26@hotmail.com>
2024-04-02 20:35:50 +0200

    HTML semantic bugs. RawSheet atoms.

--- app/(shared)/page.tsx
+++ app/(shared)/page.tsx
@@ -1,16 +1,17 @@
 "use client"
 
-import { useAtom, useAtomValue } from "jotai"
+import { useAtom, useAtomValue, useSetAtom } from "jotai"
 import { DropZone } from "@/app/components/common/drop-zone/DropZone"
 import { ShareTable } from "@/app/components/common/table/ShareTable"
 import { processExcelFile } from "@/app/(shared)/lib/excelProcessor"
 import { TotalFooter } from "@/app/components/common/table/TotalFooter"
 import { PersonInputs } from "@/app/(shared)/components/PersonInputs/PersonInputs"
 import { ShareResults } from "@/app/(shared)/components/ShareResults"
-import { transactionsAtom } from "@/app/(shared)/lib/store"
+import { rawSheetAtom, transactionsAtom } from "@/app/(shared)/lib/store"
 
 const Page = () => {
   const [transactions, setTransactions] = useAtom(transactionsAtom)
+  const setRawSheet = useSetAtom(rawSheetAtom)
 
   const onFileDropped = (data: ArrayBuffer) => {
     const workbook = processExcelFile(data)
@@ -21,44 +22,57 @@ const Page = () => {
       .getValue()
 
     setTransactions(workbook)
+
+    const rawSheet = processExcelFile(data).getFirstSheet().getValue()
+    setRawSheet(rawSheet)
+    console.log("rawSheet", rawSheet)
   }
 
   return (
-    <div className="container my-10">
-      <DropZone callback={onFileDropped} className="mb-8" />
+    <>
+      <div className="container my-10">
+        <DropZone callback={onFileDropped} className="mb-8" />
 
-      <PersonInputs />
+        <PersonInputs />
 
-      <ShareResults />
+        <ShareResults />
 
-      <br />
+        <br />
 
-      <div style={{ width: "fit-content" }}>
-        {transactions?.length > 0 && (
-          <>
-            <div className="flex gap-10 flex-wrap">
-              <ShareTable headline={"Extrakort"} transactions={transactions} labels={"Dela Ani"} />
+        <div style={{ width: "fit-content" }}>
+          {transactions?.length > 0 && (
+            <>
+              <div className="flex gap-10 flex-wrap">
+                <ShareTable
+                  headline={"Extrakort"}
+                  transactions={transactions}
+                  labels={"Dela Ani"}
+                />
 
-              <ShareTable headline="Huvudkort" transactions={transactions} labels={"Dela Magnus"} />
+                <ShareTable
+                  headline="Huvudkort"
+                  transactions={transactions}
+                  labels={"Dela Magnus"}
+                />
 
-              <ShareTable
-                className="hidden"
-                headline="Privat"
-                transactions={transactions}
-                labels={"Magnus privat"}
-              />
-            </div>
-
-            <TotalFooter
-              transactions={transactions.filter(
-                (transaction) =>
-                  transaction.labels === "Dela Ani" || transaction.labels === "Dela Magnus"
-              )}
-            />
-          </>
-        )}
+                <ShareTable
+                  className="hidden"
+                  headline="Privat"
+                  transactions={transactions}
+                  labels={"Magnus privat"}
+                />
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+
+      <TotalFooter
+        transactions={transactions.filter(
+          (transaction) => transaction.labels === "Dela Ani" || transaction.labels === "Dela Magnus"
+        )}
+      />
+    </>
   )
 }
 
---
+++ app/components/common/table/TableSummaryFooter.tsx
@@ -0,0 +1,24 @@
+import { TableFooter, TableRow, TableCell, Table } from "@/app/components/ui/table"
+import { sumTransactions } from "@/lib/transactions-utils"
+import { Transaction } from "@/types/types"
+import React from "react"
+
+interface Props {
+  transactions: Transaction[]
+}
+
+/**
+ * Combined footer for tables.
+ */
+const TableSummaryFooter = ({ transactions }: Props) => {
+  return (
+    <TableFooter>
+      <TableRow>
+        <TableCell colSpan={2}>Total</TableCell>
+        <TableCell className="text-right">{sumTransactions(transactions)}</TableCell>
+      </TableRow>
+    </TableFooter>
+  )
+}
+
+export { TableSummaryFooter }
