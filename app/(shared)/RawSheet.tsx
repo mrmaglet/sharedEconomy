51b2278f2cf1824c157039a8bec597c783a231f9
maglet <magsnus26@hotmail.com>
2024-04-04 19:19:21 +0200

    Strict null checks. First wizard step.

--- app/(shared)/RawSheet.tsx
+++ app/(shared)/RawSheet.tsx
@@ -1,43 +1,42 @@
 "use client"
 
 import { rawSheetAtom } from "@/app/(shared)/lib/store"
-import { TableCellSelect } from "@/app/components/common/select-table/TableCellSelect"
 import { TableRowSelect } from "@/app/components/common/select-table/TableRowSelect"
-import { Table, TableCell, TableHead, TableRow } from "@/app/components/ui/table"
-import { RawSheet, RawSheetRow } from "@/types/types"
+import { Button } from "@/app/components/ui/button"
+import { Table, TableCell } from "@/app/components/ui/table"
+import { H3 } from "@/app/components/ui/typography"
+import { RawSheetRow } from "@/types/types"
 import { useAtomValue } from "jotai"
 import React, { useEffect, useState } from "react"
 
 const RawSheet = () => {
   const rawSheet = useAtomValue(rawSheetAtom)
   const [selectedRow, setSelectedRow] = useState<number | null>(null)
-
-  useEffect(() => {
-    console.log(selectedRow)
-  }, [])
+  const [wizard, setWizard] = useState<"Idle" | "SelectRow" | "SelectSumCol" | "Done">("Idle")
 
   if (!rawSheet) return
 
-  const handleRowClick = (row: number) => {
-    console.log(row, "row")
-
-    setSelectedRow((prev) => (prev === row ? null : row))
-  }
-
   return (
-    <Table>
-      {rawSheet.map((row: RawSheetRow, i: number) => (
-        <TableRowSelect
-          onClick={() => handleRowClick(i)}
-          key={i}
-          className={`${selectedRow === i ? "bg-amber-500 hover:bg-amber-500" : ""} `}
-        >
-          {row.map((cell: string | number, j: number) => (
-            <TableCell key={`${i}-${j}`}>{cell}</TableCell>
-          ))}
-        </TableRowSelect>
-      ))}
-    </Table>
+    <>
+      <div className="flex justify-between mt-6 mb-12">
+        <H3>Select your header row</H3>
+        <Button>Next</Button>
+      </div>
+
+      <Table>
+        {rawSheet.map((row: RawSheetRow, i: number) => (
+          <TableRowSelect
+            onClick={() => setSelectedRow((prev) => (prev === i ? null : i))}
+            key={i}
+            className={`${selectedRow === i ? "bg-blue-100 hover:bg-blue-100 font-bold" : ""} `}
+          >
+            {row.map((cell: string | number, j: number) => (
+              <TableCell key={`${i}-${j}`}>{cell}</TableCell>
+            ))}
+          </TableRowSelect>
+        ))}
+      </Table>
+    </>
   )
 }
 
