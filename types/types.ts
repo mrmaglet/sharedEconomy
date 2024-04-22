99c5bf45d1eeedc494e6b50aaa39544f342cab5a
maglet <magsnus26@hotmail.com>
2024-04-22 22:05:31 +0200

    wip

--- types/types.ts
+++ types/types.ts
@@ -1,14 +1,19 @@
-export type Col = "firstCol" | "secondCol" | "thirdCol"
-
-export type Labels = "Dela Ani" | "Dela Magnus" | "Magnus privat"
-
-export type Transaction = {
-  date: string
-  description: string
-  amount: number
-  labels: Labels
-}
-
-/** Parsed json from excel file */
+/**
+ * Parsed json from excel file
+ */
 export type RawSheet = RawSheetRow[]
 export type RawSheetRow = (string | number)[]
+
+/**
+ * Config
+ */
+type Config = {
+  name: string
+  files: Files[]
+}
+
+type Files = {
+  name: string
+  file: RawSheet
+  header: number | undefined
+}
